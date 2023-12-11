43a79acde0976146c2f732812617fd03d0bbddaa
PKomandur01 <86378984+PKomandur01@users.noreply.github.com>
2023-12-11 16:11:00 -0600

    Add files via upload

---
+++ Article.java
@@ -0,0 +1,14 @@
+class Article{
+    int id;
+    String title, author;
+    
+    public Article(int i, String t, String a){
+        id = i;
+        title = t;
+        author = a;
+        
+    }
+    public String toString(){
+        return String.format("\t %d | %s | %s-->\n", id, title, author);
+    }
+}
